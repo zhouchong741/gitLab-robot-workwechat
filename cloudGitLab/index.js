348a7271f5896ee9b70303078fb4440c3769ef51
周冲 <zhouchong741@gmail.com>
2021-06-22 13:26:07 +0800

    update
    修改推送样式

--- cloudGitLab/index.js
+++ cloudGitLab/index.js
@@ -19,12 +19,20 @@ async function handlePush(body, robotid) {
     const robot = new ChatRobot(
         robotid
     );
-    let {user_name, project:{name:proName, web_url}, commits} = body;
-    const lastCommit = commits[0];
-    const mdMsg = ` 项目 [${proName}](${web_url}) 收到一次push提交
-                    提交者:  ${user_name}
-                    commitID: ${lastCommit.id}
-                    最新提交信息: ${lastCommit.message}`;
+    let {ref, user_name, project:{name:proName, web_url}, commits} = body;
+    // 循环处理 commits
+    var commitListString = '';
+    for (var i = 0; i < commits.length; i++) {
+        commitListString +=(i+1) + ') ' + commits[i].timestamp.substring(0, 19).replace('T',' ') + ' ' + commits[i].title + ' ' + commits[i].author.name + '\n';
+    }
+    // ref 分支名称 为：refs/heads/XXXXXX 取 XXXXXX 即可
+    const brunch = ref.slice(11);
+    const commitsDetail = 'http://git-inc.ovopark.com:6780/android/ovopark/-/commits/' + brunch;
+    const mdMsg = `项目 [${proName}](${web_url}) 收到一次push提交\n
+        >分支： <font color=\"comment\">${brunch}</font>
+        >提交者:  <font color=\"comment\">${user_name}</font>
+        >提交信息: 共计<font color=\"info\">${commits.length}</font>条\n<font color=\"comment\">${commitListString}</font>
+        >[查看详情](${commitsDetail})`;
     await robot.sendMdMsg(mdMsg);
     return mdMsg;
 }
@@ -39,11 +47,11 @@ async function handlePR(body, robotid) {
         robotid
     );
     let {object_kind='', user:{name, avatar_url}, project:{name:proName, web_url}, object_attributes:{title, state, target_branch, source_branch, url}} = body;
-    const mdMsg = `[${name}](${avatar_url})在 [${proName}](${web_url}) 中${state}了一次${object_kind}
-                    标题：${title}
-                    源分支：${source_branch}
-                    目标分支：${target_branch}
-                    [查看PR详情](${url})`;
+    const mdMsg = `[${name}](${avatar_url})在 [${proName}](${web_url}) 中${state}了一次${object_kind}\n
+        >标题： <font color=\"comment\">${title}</font>
+        >源分支： <font color=\"comment\">${source_branch}</font>
+        >目标分支： <font color=\"comment\">${target_branch}</font>
+        >[查看PR详情](${url})`;
     await robot.sendMdMsg(mdMsg);
     return mdMsg;
 }
@@ -58,10 +66,10 @@ async function handleIssue(body, robotid) {
         robotid
     );
     let {user: {name, avatar_url}, project: {name:proName ,web_url}, object_attributes: {title, url, action}} = body;
-    const mdMsg = `[${name}](${avatar_url}) 在 [${proName}](${web_url}) 中 ${action} 了一个issue
-                    标题：${title}
-                    发起人：[${name}](${avatar_url})
-                    [查看详情](${url})`;
+    const mdMsg = `[${name}](${avatar_url}) 在 [${proName}](${web_url}) 中 ${action} 了一个issue\n
+        >标题： <font color=\"comment\">${title}</font>
+        >发起人： <font color=\"comment\">[${name}](${avatar_url})</font>
+        >[查看详情](${url})`;
     await robot.sendMdMsg(mdMsg);
     return mdMsg;
 }
